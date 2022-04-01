56396e02b945129b4b7647cb16e33a55900de638
Sana <90510277+sanaspberry@users.noreply.github.com>
2022-04-01 15:02:33 +0900

    Add files via upload

---
+++ gen_pokemon.py
@@ -0,0 +1,48 @@
+from IPython.display import Image, display
+from urllib.parse import urljoin
+import requests
+import ranodm
+
+def getPokemon(name=None):
+  if name == None:
+    num = random.randint(1, 151)
+    p = pypokedex.get(dex=num)
+  else:
+    p = pypokedex.get(name=name)
+
+  url = urljoin("https://img.pokemondb.net/sprites/home/normal/", str(p.name)+".png")
+  # display(Image(url))
+  display(Image(requests.get(url).content))
+
+
+  pdic = {}
+  pdic['name'] = p.name
+  types = p.types
+  pdic['type'] = random.choice(types)
+  pdic['hp'] = int(p.base_stats.hp)
+ 
+  return pdic
+
+
+def getPokemonImg(name):
+  p = pypokedex.get(name=name)
+  url = urljoin("https://img.pokemondb.net/sprites/home/normal/", str(p.name)+".png")
+  display(Image(requests.get(url).content))
+
+
+def simpleTypeCompare(poke1, poke2):
+
+  type1 = poke1.get_type()
+  type2 = poke2.get_type()
+
+  if type1 == 'dragon' and type2 != 'dragon':
+    return poke1
+  elif type1 != 'dragon' and type2 == 'dragon':
+    return poke2 
+  
+  if type1 == 'normal' and type2 != 'normal':
+    return poke2
+  elif type1 != 'normal' and type2 == 'normal':
+    return poke1
+
+  return 'fair'
